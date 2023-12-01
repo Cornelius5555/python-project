7089eb1653ed9f353aa65ce9407ec152e5270da1
Cornelius5555 <corneliuskiplagat5@gmail.com>
2023-12-01 10:31:45 +0300

    update

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 #Write a program that prompts the user to enter the base and height of a triangle and returns its area.
 base = int(input("Enter base: "))
 height = int(input("Enter height: "))
-area = base * height
+area = (base * height) / 2
 print(f"The area is: {area}")
--- task14.py
+++ task14.py
@@ -2,4 +2,14 @@
 Write a program that takes input of 2 values and adds them. The program should only accept numbers and floats only or otherwise display 
 an error “invalid character entered” and take
 the user to re-enter the inputs ."""
-#for i in num1 
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        break
+    except ValueError:
+        print("Invalid character entered. Please enter a number or float.")
+
+sum = num1 + num2
+print(sum)
+
